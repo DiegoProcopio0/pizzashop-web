bc524e71382ed6e1213eec7d0b393b0778cb6e9c
Diego Procopio <diegprocopio98@gmail.com>
2024-08-30 13:45:52 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
+export function App() {
+  return (
+    <>
+      <div className="w-full flex justify-center items-center h-screen bg-cyan-900">
+        <Button>Enviar</Button>
+      </div>
+    </>
+  )
+}
