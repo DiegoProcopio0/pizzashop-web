bf8c008f4c548f5a7b8f7effd470f98f1e7c56a0
Diego Procopio <diegprocopio98@gmail.com>
2024-09-02 13:31:08 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { Button } from './components/ui/button'
-import './global.css'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import "./global.css";
 
 export function App() {
-  return (
-    <>
-      <div className="w-full flex justify-center items-center h-screen bg-cyan-900">
-        <Button>Enviar</Button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
