c7a2e3745d28d4d52e4756564e81d7bb55684499
Diego Procopio <diegprocopio98@gmail.com>
2024-09-03 21:58:13 -0300

    eip

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,64 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@radix-ui/react-label'
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+const sigInForm = z.object({
+  email: z.string().email(),
+})
+
+type SigInForm = z.infer<typeof sigInForm>
+
 export function SignIn() {
-  return <h1>SignIn</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SigInForm>()
+
+  async function handlerSigIn(data: SigInForm) {
+    console.log(data)
+
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    toast.success('Enviamos um link de autentificação para seu e-mail.', {
+      action: { label: 'Reenviar', onClick: () => handlerSigIn(data) },
+    })
+  }
+
+  return (
+    <>
+      <Helmet title="login" />
+      <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="sign-up">Novo Estabelecimento</Link>
+        </Button>
+
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="gap- flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+          <form onSubmit={handleSubmit(handlerSigIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar Painel
+            </Button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
