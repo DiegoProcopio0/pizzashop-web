cc79df4a9ce8373e5a61e555de3721a825dc5c8b
Diego Procopio <diegprocopio98@gmail.com>
2024-09-11 13:33:46 -0300

    wip

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,15 +1,8 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
-import { ArrowRight, Search, X } from 'lucide-react'
+import { Table, TableBody, TableHead, TableHeader } from '@/components/ui/table'
 import { Helmet } from 'react-helmet-async'
+import { OrderTableRow } from './order-table-row'
+import { OrderTableFilters } from './order-table-filters'
+import { Pagination } from '@/components/pagination'
 
 export function Orders() {
   return (
@@ -17,70 +10,29 @@ export function Orders() {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-medium">Filtros</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableHead className="w-[64px]"></TableHead>
-              <TableHead className="w-[140px]">Identificador</TableHead>
-              <TableHead className="w-[180px]">Realizada há</TableHead>
-              <TableHead className="w-[140px]">Status</TableHead>
-              <TableHead>Cliente</TableHead>
-              <TableHead className="w-[140px]">Total do Pedido</TableHead>
-              <TableHead className="w-[164px]"></TableHead>
-              <TableHead className="w-[132px]"></TableHead>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, index) => {
-                return (
-                  <TableRow key={index}>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <Search className="size-3" />
-                        <span className="sr-only">Detalhes do pedido</span>
-                      </Button>
-                    </TableCell>
-                    <TableCell className="font-mono text-sm font-medium">
-                      scssdssdsddsssd
-                    </TableCell>
-                    <TableCell className="text-muted-foreground">
-                      Há 15 minutos
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex items-center gap-2">
-                        <span className="size-2 rounded-full bg-slate-400" />
-                        <span className="font-medium text-muted-foreground">
-                          Pendente
-                        </span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="font-medium">
-                      Diego Procopio da Silva
-                    </TableCell>
-                    <TableCell className="font-medium">R$ 149.90</TableCell>
-                    <TableCell>
-                      <Button variant="outline" size="xs">
-                        <ArrowRight className="mr-2 size-3" />
-                        Aprovado
-                      </Button>
-                    </TableCell>
-                    <TableCell>
-                      <Button variant="ghost" size="xs">
-                        <X className="mr-2 size-3" />
-                        Cancelar
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )
-              })}
-            </TableBody>
-          </Table>
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableHead className="w-[64px]"></TableHead>
+                <TableHead className="w-[140px]">Identificador</TableHead>
+                <TableHead className="w-[180px]">Realizada há</TableHead>
+                <TableHead className="w-[140px]">Status</TableHead>
+                <TableHead>Cliente</TableHead>
+                <TableHead className="w-[140px]">Total do Pedido</TableHead>
+                <TableHead className="w-[164px]"></TableHead>
+                <TableHead className="w-[132px]"></TableHead>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, index) => {
+                  return <OrderTableRow key={index} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} perPage={10} totalCount={150} />
         </div>
       </div>
     </>
