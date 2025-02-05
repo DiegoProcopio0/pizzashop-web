c7f442431f5f7bc7f01473944bb73608cafb8911
Diego Procopio <diegprocopio98@gmail.com>
2025-02-05 13:37:09 -0300

    wip

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -8,14 +8,18 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { getProfile } from '@/api/get-profile'
 import { getManagerRestaurant } from '@/api/get-manager-restaurant'
 import { Skeleton } from './ui/skeleton'
 import { Dialog, DialogTrigger } from './ui/dialog'
 import { StoreProfileDialog } from './store-profile-dialog'
+import { signOut } from '@/api/sign-out'
+import { useNavigate } from 'react-router-dom'
 
 export function AccountMenu() {
+  const navigate = useNavigate()
+
   const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
@@ -28,6 +32,13 @@ export function AccountMenu() {
       queryFn: getManagerRestaurant,
       staleTime: Infinity,
     })
+
+  const { mutateAsync: signOutFn, isPending: isSigninOut } = useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      navigate('/sign-in', { replace: true })
+    },
+  })
 
   return (
     <Dialog>
@@ -67,9 +78,15 @@ export function AccountMenu() {
               <Building className="mr-2 size-4" />
               <span>Perfil da lojas</span>
             </DropdownMenuItem>
-            <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-              <LogOut className="mr-2 size-4" />
-              <span>Sair</span>
+            <DropdownMenuItem
+              asChild
+              className="text-rose-500 dark:text-rose-400"
+              disabled={isSigninOut}
+            >
+              <button className="w-full" onClick={() => signOutFn()}>
+                <span>Sair</span>
+                <LogOut className="mr-2 size-4" />
+              </button>
             </DropdownMenuItem>
           </DialogTrigger>
         </DropdownMenuContent>
